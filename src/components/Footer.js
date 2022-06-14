94757210fd196766adb618d6107cafb1b73a7f67
Tomas Mehdi <tomymehdi@gmail.com>
2022-06-14 23:36:31 +0200

    remove text over banner

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Link } from "gatsby";
 
 // import logo from "../img/logo.svg";
-import logo2 from "../img/logo.png";
+// import logo2 from "../img/logo.png";
 import facebook from "../img/social/facebook.svg";
 import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
@@ -19,11 +19,11 @@ const Footer = class extends React.Component {
             style={{ width: "14em", height: "10em" }}
           /> */}
           <div>EarthHumans</div>
-          <img
+          {/* <img
             src={logo2}
             alt="EarthHumans"
             style={{ width: "14em", height: "10em" }}
-          />
+          /> */}
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
--- src/components/FullWidthImage.js
+++ src/components/FullWidthImage.js
@@ -55,51 +55,7 @@ export default function FullWidthImage(props) {
             formats={["auto", "webp", "avif"]}
           />
         )}
-        {(title || subheading) && (
-          <div
-            style={{
-              // By using the same grid area for both, they are stacked on top of each other
-              gridArea: "1/1",
-              position: "relative",
-              // This centers the other elements inside the hero component
-              placeItems: "center",
-              display: "grid",
-            }}
-          >
-            {/* Any content here will be centered in the component */}
-            {title && (
-              <h1
-                className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-                style={{
-                  boxShadow:
-                    "rgb(173, 65, 30) 0.5rem 0px 0px, rgb(173, 65, 30) -0.5rem 0px 0px",
-                  backgroundColor: "rgb(173, 65, 30)",
-                  color: "white",
-                  lineHeight: "1",
-                  padding: "0.25em",
-                }}
-              >
-                {title}
-              </h1>
-            )}
-            {subheading && (
-              <h3
-                className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-                style={{
-                  boxShadow:
-                    "rgb(173, 65, 30) 0.5rem 0px 0px, rgb(173, 65, 30) -0.5rem 0px 0px",
-                  backgroundColor: "rgb(173, 65, 30)",
-                  color: "white",
-                  lineHeight: "1",
-                  padding: "0.25rem",
-                  marginTop: "0.5rem",
-                }}
-              >
-                {subheading}
-              </h3>
-            )}
-          </div>
-        )}
+        
       </div>
     </React.Fragment>
   );
